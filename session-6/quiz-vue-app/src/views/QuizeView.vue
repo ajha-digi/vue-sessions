b3bbc4b5666c610421f04ae576476f0c035d62d3
ajha-digi <ajha@digisprint.com>
2023-07-20 11:05:07 +0530

    Render options and fixed bar percentage.

--- session-6/quiz-vue-app/src/views/QuizeView.vue
+++ session-6/quiz-vue-app/src/views/QuizeView.vue
@@ -1,6 +1,9 @@
 <script setup>
+    import {ref, watch, computed} from "vue";
     import {useRoute} from "vue-router";
     import quizes from "../data/quizes.json";
+    import Question from "../components/Question.vue";
+    import QuizHeader from "../components/QuizHeader.vue";
 
     const route = useRoute();
     const quizId = parseInt(route.params.id);
@@ -8,112 +11,28 @@
     const quiz = quizes.find(q => q.id === quizId);
 
     console.log(quiz);
+    const currentQuestionIndex = ref(0);
+
+    // const questionStatus = ref(`${currentQuestionIndex.value}/${quiz.questions.length}`);
+
+    // watch(()=>currentQuestionIndex.value, ()=> {
+    //     questionStatus.value = `${currentQuestionIndex.value}/${quiz.questions.length}`;
+    // })
+
+    const questionStatus = computed(()=> `${currentQuestionIndex.value}/${quiz.questions.length}`);
+    const barPercentage = computed(()=> `${currentQuestionIndex.value/quiz.questions.length * 100}%`);
+
 </script>
 
 <template>
    <div>
-    <header>
-        <h4> Question 1/3</h4>
-        <div class="bar">
-            <div class="completion"></div>
-        </div>
-    </header>
-
+    <QuizHeader :questionStatus="questionStatus"  :barPercentage="barPercentage" />
     <div>
-        <div class="question-container">
-            <h1 class="question">
-                chemical name of table salt
-            </h1>
-        </div>
-        <div class="options-container">
-            <div class="option">
-                <p class="option-label">A</p>
-                <div class="option-value">
-                    <p>Nacl</p>
-                </div>
-            </div>
-
-            <div class="option">
-                <p class="option-label">A</p>
-                <div class="option-value">
-                    <p>Nacl</p>
-                </div>
-            </div>
-
-            <div class="option">
-                <p class="option-label">A</p>
-                <div class="option-value">
-                    <p>Nacl</p>
-                </div>
-            </div>
-
-            <div class="option">
-                <p class="option-label">A</p>
-                <div class="option-value">
-                    <p>Nacl</p>
-                </div>
-            </div>
-            
-        </div>
+        <Question :question="quiz.questions[currentQuestionIndex]"/>
     </div>
-
+    <button @click="currentQuestionIndex++" >Next Question</button>
    </div>
 </template>
 
 <style scoped>
-    header {
-        margin-top: 20px;
-
-    }
-
-    header h4 {
-        font-size: 30px;
-    }
-
-    .bar {
-        width: 300px;
-        height: 50px;
-        border: 3px solid bisque;
-    }
-
-    .completion {
-        height: 100%;
-        width: 100%;
-        background-color: bisque;
-    }
-
-
-.question-container{
-    margin-top: 20px;
-}
-
-.question {
-    font-size: 40px;
-    margin-bottom: 20px;
-}
-
-.option {
-    display: flex;
-    margin-bottom: 20px;
-    cursor: pointer;
-}
-
-.option-label {
-    background-color: bisque;
-    width: 50px;
-    height: 50px;
-    font-style: 30px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-}
-
-.option-value {
-    width: 60%;
-    font-style: 30px;
-    padding: 10px;
-    background-color: rgb(244, 239, 239);
-}
-
-
 </style>
