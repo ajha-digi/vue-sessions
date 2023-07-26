6b3d5a7d84656663b22c7ef4a1fa899e7b09eefb
ajha-digi <ajha@digisprint.com>
2023-07-26 11:00:24 +0530

    Implemented Axios call

---
+++ session-7/api-call-app/src/App.vue
@@ -0,0 +1,51 @@
+<script setup>
+import { ref } from "vue";
+import axios from "axios";
+
+const posts = ref([]);
+const title = ref("");
+const author = ref("");
+
+  const getAllPost = async () => {
+    const {data} = await axios.get(`http://localhost:3000/posts`);
+    posts.value = data;
+  }
+
+  getAllPost()
+
+  const sendPost = async () => {
+    const payload = { 
+      "id": posts.value.length + 1, 
+      "title":title.value, 
+      "author": author.value
+    }
+    await axios.post(`http://localhost:3000/posts`,payload);
+    title.value = "";
+    author.value=""
+    getAllPost()
+  }
+
+  const deletePost = async (id) => {
+    await axios.delete(`http://localhost:3000/posts/${id}`)
+    getAllPost()
+  }
+
+</script>
+
+<template>
+  <h1>Posts</h1>
+  <div>
+    <input v-model="title"/>
+    <input v-model="author"/>
+    <button type="submit" @click="sendPost">Save</button>
+  </div>
+  <div>
+    <ul v-for="post in posts" :key="post.id">
+      <li>{{post.title}} <span @click="deletePost(post.id)">X</span></li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
